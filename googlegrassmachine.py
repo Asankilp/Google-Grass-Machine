5e2de13877826ce0842236d2debadbc5df7c8694
Asankilp <3384816498@qq.com>
2020-07-20 11:20:28 +0800

    update

--- googlegrassmachine.py
+++ googlegrassmachine.py
@@ -1,7 +1,7 @@
 #谷歌全自动生草机 by Asankilp
-#输入的句子将自动检测语言，并进行生草。
+#输入的句子将自动检测语言，并进行重复翻译。
 #请使用Python3运行此程序，使用前请安装googletrans和openssl。
-#使用命令：pip install googletrans , pip install openssl
+#使用命令：pip install googletrans , pip install openssl来安装依赖包。
 from googletrans import Translator
 translator = Translator(service_urls=['translate.google.cn']) #将翻译服务URL替换为国内区域名
 src2 = input('请输入原句：')
@@ -11,4 +11,4 @@ for mubiao in lang:
     count = count + 1 #翻译计次
     src2 = translator.translate(src2,dest=mubiao).text
     print(str(count)+"次翻译结果："+src2)
-print("最终翻译结果:"+src2)
+print("最终翻译结果："+src2)
